319e0d2ac5fbe0efd096d9993674c8a421f31642
molinax18 <arielmolina8953@gmail.com>
2026-09-09 01:30:22 -0300

    chore: replace ts-node for tsx package

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { connectDatabase } from "./database.ts";
+import { connectDatabase } from "./database.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
